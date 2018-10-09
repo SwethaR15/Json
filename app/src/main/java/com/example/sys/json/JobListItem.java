1d3b0f33d35061f6ccad79ed6ceb59abfd11575c
SwethaR15 <41993382+SwethaR15@users.noreply.github.com>
2018-10-08 23:58:58 -0700

    Json

---
+++ app/src/main/java/com/example/sys/json/JobListItem.java
@@ -0,0 +1,42 @@
+package com.example.sys.json;
+
+class JobListItem {
+    private String StudentName;
+    private String College;
+    private String Branch;
+    private String Rollno;
+
+
+
+
+
+    public String getStudentName() {
+        return StudentName;
+    }
+
+    public void setStudentName(String studentName) {
+        StudentName = studentName;
+    }
+
+    public String getCollege() {
+        return College;
+    }
+
+    public void setCollege(String college) {
+        College = college;
+    }
+    public String getBranch() {
+        return Branch;
+    }
+
+    public void setBranch(String branch) {
+        Branch = branch;
+    }
+    public String getRollno() {
+        return Rollno;
+    }
+
+    public void setRollno(String rollno) {
+        Rollno = rollno;
+    }
+}
